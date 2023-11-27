efed718f40df9271be226457e64c3bcb6dae665e
Daniel Chimen <daniel.chimen@gmail.com>
2023-11-27 12:39:32 +0000

    updates

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -26,69 +26,59 @@ export const Filters = () => {
   const handleStopsChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value, checked } = e.target;
 
-    setFilters({
-      ...filters,
-      stops: filters.stops.map((item) => {
-        if (value === "0") {
-          if (!checked && Number(item.value) > 0 && item.checked) {
-            console.log(
-              " 1 stop or more selected and direct is clicked, so uncheck all."
-            );
-            return {
-              ...item,
-              checked: false,
-            };
-          }
-        }
-
-        if (value === "1") {
-          if (checked && Number(item.value) === 0 && !item.checked) {
-            console.log(
-              "1 stop selected and direct is not checked, so check it."
-            );
-            return {
-              ...item,
-              checked: true,
-            };
-          }
-
-          if (checked && Number(item.value) === 2 && item.checked) {
-            console.log("if direct and 2 stops are checked, uncheck 2 stops.");
-            return {
-              ...item,
-              checked: false,
-            };
-          }
-
-          if (!checked && Number(item.value) === 2 && item.checked) {
-            console.log('set remainChecked to "true"');
-            return {
-              ...item,
-              checked: false,
-            };
-          }
-        }
-
-        if (value === "2") {
-          if (checked && Number(item.value) < 2 && !item.checked) {
-            console.log(
-              "2 stops selected and previous checkboxes are unchecked, so check them."
-            );
-            return {
-              ...item,
-              checked: true,
-            };
-          }
-        }
-
-        if (item.value === value) {
+    const updatedStops = filters.stops.map((item) => {
+      if (value === "0" && !checked) {
+        if (Number(item.value) > 0 && item.checked) {
+          // Up to 1 Stop or more are selected and Direct is clicked, so uncheck all.
           return {
             ...item,
-            checked,
+            checked: false,
           };
         }
-        return item;
-      }),
+      }
+
+      if (value === "1") {
+        if (checked) {
+          if (Number(item.value) === 0 && !item.checked) {
+            // Up to 1 Stop is checked and Direct is not checked, so check Direct.
+            return {
+              ...item,
+              checked: true,
+            };
+          }
+        } else {
+          if (Number(item.value) === 2 && item.checked) {
+            // Up to 2 Stops is checked and Up to 1 Stop is unchecked, so uncheck Up to 2 Stops.
+            return {
+              ...item,
+              checked: false,
+            };
+          }
+        }
+      }
+
+      if (value === "2" && checked) {
+        if (Number(item.value) < 2 && !item.checked) {
+          // Up to 2 Stops selected and previous checkboxes are unchecked, so check them.
+          return {
+            ...item,
+            checked: true,
+          };
+        }
+      }
+
+      if (item.value === value) {
+        return {
+          ...item,
+          checked,
+        };
+      }
+      return item;
+    });
+
+    setFilters({
+      ...filters,
+      stops: updatedStops,
     });
   };
 
